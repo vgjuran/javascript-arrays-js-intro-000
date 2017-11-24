d5f730578f750f212144832becbdb3ada6043568
Vladimir <vgjuran@protonmail.com>
2017-11-24 09:42:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function addElementToBeginningOfArray(array, element){
 
 function destructivelyAddElementToBeginningOfArray(array, element){
 
-  array=[...element, array];
+  array=[array, ...element];
   return array;
 
 }
