5f0f79579b109ee54d6032a275200a6bb024397c
Vladimir <vgjuran@protonmail.com>
2017-11-24 10:43:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,8 @@ function accessElementInArray(array, index){
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array, index){
-  array.shift(index);
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
   return array;
 }
 
@@ -44,6 +44,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length - 1)
+  array.slice(0, array.length - 1);
   return array;
 }
