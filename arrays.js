002077939e9eb1fd631fc7bfb4d177abd1a997ea
Vladimir <vgjuran@protonmail.com>
2017-11-24 09:51:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 
 function addElementToEndOfArray(array, element){
   return [...array, element];
-  
+
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
